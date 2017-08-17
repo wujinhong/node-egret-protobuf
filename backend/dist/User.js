de18225669bbb94d7a96492a4f736788cf8f6e58
Gordon <597785841@qq.com>
2017-08-17 16:55:13 +0800

    修改编译参数

---
+++ backend/dist/User.js
@@ -0,0 +1,14 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+class User {
+    constructor(ws) {
+        this.game_status = 0;
+        this.id = 0;
+        this.name = "new";
+        this.start = 0;
+        this.alive = 1;
+        this.ws = ws;
+    }
+}
+exports.User = User;
+//# sourceMappingURL=User.js.map
